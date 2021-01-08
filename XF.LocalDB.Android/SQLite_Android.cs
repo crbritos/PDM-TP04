6ad564716e6eb5c527e8a8b7f4f65f4ed4a63637
crbritos <53920244+crbritos@users.noreply.github.com>
2021-01-08 18:57:21 -0300

    Add files via upload

---
+++ XF.LocalDB.Android/SQLite_Android.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using XF.LocalDB;
+using XF.LocalDB.Android;
+using XF.LocalDB.Data;
+using System.IO;
+using Xamarin.Forms;
+using SQLitePCL;
+
+[assembly: Dependency(typeof(SQLite_Android))]
+namespace XF.LocalDB.Android {
+    public class SQLite_Android : Data.ISQLite {
+        public SQLite_Android() {
+        }
+        public SQLite.SQLiteConnection GetConexao() {
+            var arquivodb = "ifspdb.db3";
+            string caminho = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var local = Path.Combine(caminho, arquivodb);
+            var conexao = new SQLite.SQLiteConnection(local);
+            return conexao;
+        }
+    }
+}
---
+++ XF.LocalDB/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using XF.LocalDB.ViewModel;
+
+namespace XF.LocalDB
+{
+	public partial class App : Application
+	{
+		public App() { // The root page of your application
+			MainPage = new NavigationPage(new View.Aluno.MainPage());
+		}
+		static Model.Aluno alunoModel;
+		public static Model.Aluno AlunoModel {
+			get {
+				if (alunoModel == null) {
+					alunoModel = new Model.Aluno();
+				} return alunoModel;
+			}
+		}
+	}
+}
